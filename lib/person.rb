f06c64875f674156989fab0fc960410eabb543e4
Shau Yin Luk <ivan.luk@icloud.com>
2020-03-31 17:13:24 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(jobname)
+        @job = jobname
+    end
+
+    def job
+        @job
+    end
+
+end
