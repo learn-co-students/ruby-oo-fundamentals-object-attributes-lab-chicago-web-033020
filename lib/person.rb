a64f2e9c1fe866b4d19e59def52a883041b44a9b
Auliz <joseph.auz77@gmail.com>
2020-03-31 23:51:33 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+
+class Person 
+
+    def initialize 
+    end 
+
+    def name=(p_name)
+        @name = p_name 
+    end 
+
+    def name 
+        @name
+    end 
+
+    def job=(p_job)
+        @job = p_job
+    end 
+
+    def job 
+        @job
+    end 
+
+
+end 
