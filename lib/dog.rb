d7f1f4e527cc3533edda21829dc2c2f36beac77d
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-01 03:30:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  def initializaton(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(teach)
+    @breed = teach
+  end
+end
+
+class Dog
+  def initializaton(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(teach)
+    @name = teach
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+  def initialization(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(teach)
+    @job = teach
+  end
+end
+   
+class Person
+  def initialization(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(teach)
+    @name = teach
+  end
+end
